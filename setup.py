01d8caf938da0cdd7361b2b27b8aacdf94351bfb
taylor <tayjaybabee@gmail.com>
2020-09-09 11:39:20 -0500

    Version bump
    Modified:
    - inspy_logger/\_\_init\_\_.py:
        - Added version checking logic
        - Added arguments to start function of the InspyLogger class for controlling noisy output
    
    - pyproject.toml:
        - Version bump 2.0.0a3 -> 2.0.0a4
    
    - setup.py:
        - Version bump 2.0.0a3 -> 2.0.0a4
    
    - .gitignore:
        - Added IDE settings file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="inSPy-Logger",
-    version="2.0.0-alpha.3",
+    version="2.0.0-alpha.4",
     packages=["inspy_logger"],
     url="https://softworks.inspyre.tech/inSPy-Logger",
     license="GNU",
