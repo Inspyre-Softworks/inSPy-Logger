8dc4be48749646a1e9f20ab727b1eca61483eb94
taylor <tayjaybabee@gmail.com>
2020-09-09 12:50:16 -0500

    Add dependency, version bump
    - Version bump v2.0.0a4 -> v2.0.0a5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="inSPy-Logger",
-    version="2.0.0-alpha.4",
+    version="2.0.0-alpha.5",
     packages=["inspy_logger"],
     url="https://softworks.inspyre.tech/inSPy-Logger",
     license="GNU",
