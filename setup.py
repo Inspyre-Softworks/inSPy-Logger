9ac27c20bb78d12a45a817fc6514e3b2d80a663e
taylor <tayjaybabee@gmail.com>
2020-09-09 23:41:25 -0500

    Add missing depenendcies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="inSPy-Logger",
-    version="2.0.0-alpha.5",
+    version="2.0.0-alpha.6",
     packages=["inspy_logger"],
     url="https://softworks.inspyre.tech/inSPy-Logger",
     license="GNU",
