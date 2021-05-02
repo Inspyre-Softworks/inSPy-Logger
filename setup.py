2b0f71dd4516f652f4f246369547c86cfdd55415
Taylor B <tayjaybabee@gmail.com>
2021-05-02 09:32:05 -0500

    * Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="inSPy-Logger",
-    version="2.1alpha12",
+    version="2.1a13",
     packages=["inspy_logger"],
     url="https://softworks.inspyre.tech/inSPy-Logger",
     license="WTFPL",
