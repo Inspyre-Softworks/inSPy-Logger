5f14119522007fbb082a7c13b1bf1a67c18586d4
Taylor B <tayjaybabee@gmail.com>
2023-12-14 19:51:58 -0600

    3.0 dev.3 (#64)
    * (tag) Version bump 3.0-dev1 -> 3.0-dev2
    
    * Updated requirements.txt
    
    * (add) Added 'inspy-logger-tool' script.
    
    * Added ability to silence InspyLogger on import.
    
    * chore(version): update version to 3.0-dev.3
    
    Update the version number in `version.py` and `pyproject.toml` to 3.0-dev.3.
    
    The updated version reflects the latest development state of the project.
    
    * refactor(helpers): refactor Loggable class and add base_classes module
    
    The Loggable class in the helpers module has been refactored to improve its functionality and readability. The class now inherits from a new LoggableDescriptor class defined in the base_classes module. This descriptor allows for easy access to a logger specific to a class method. Additionally, a new _get_parent_logging_device() function has been added to determine the parent logging device by inspecting the caller's log_device or parent_log_device attribute.
    
    These changes improve the logging capabilities of the Loggable class and enhance consistency in logging behavior and hierarchy.
    
    * feat(examples): add loggable_class and silent_import examples
    
    Add two new example files, loggable_class.py and silent_import.py, to the
    examples/v3.0 directory. These files demonstrate how to use the Loggable class
    and the silent import feature of the logger.
    
    Both files contain code that sets up a logger instance and logs debug and info
    messages. The loggable_class.py file also includes a LoggedClass class that
    extends the Loggable class and demonstrates how to use a child logger within a
    class.
    
    This addition enhances the examples provided in the project and helps users
    understand how to use the logger effectively.
    
    Issue: 3.0-DEV.3
    
    * refactor(docs): add Sphinx documentation files
    
    Added the following files to the docs folder:
    - Makefile
    - make.bat
    - index.rst
    - conf.py
    
    These files are necessary for generating the Sphinx documentation for the Inspy-Logger project.
    
    * 'Refactored by Sourcery' (#65)
    
    Co-authored-by: Sourcery AI <>
    
    * Update python-package.yml
    
    * Update python-package.yml
    
    * Update python-package.yml
    
    * Update python-package.yml
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

---
+++ docs/conf.py
@@ -0,0 +1,52 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+project = 'inspy-logger'
+copyright = '2023, Inspyre-Softworks, Taylor B.'
+author = 'Inspyre-Softworks, Taylor B.'
+release = '3.0.dev.3'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.coverage',
+    'sphinx_rtd_theme',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.argdoc',
+    'autoclasstoc',
+    'sphinx_autodoc_annotation',
+]
+autosummary_generate = True
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autodoc_default_options = {
+    'members': True,
+    'special-members': True,
+    'undoc-members': True,
+    'inherited-members': True,
+    'exclude-members': '__weakref__',
+    'private-members': True,
+
+}
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
--- inspy_logger/__init__.py
+++ inspy_logger/__init__.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
 import inspect
+import os
 import logging
 from rich.logging import RichHandler
 from inspy_logger.__about__ import __prog__ as PROG_NAME
+from inspy_logger.helpers import find_variable_in_call_stack
+
+
+# Let's set up some constants.
+LEVEL_MAP = [
+    ('debug', logging.DEBUG),
+    ('info', logging.INFO),
+    ('warning', logging.WARNING),
+    ('error', logging.ERROR),
+    ('critical', logging.CRITICAL),
+    ('fatal', logging.FATAL),
+]
+"""
+List[Tuple[str, int]]:
+    A list of tuples containing the name of a logging level and it's corresponding logging level constant.
+"""
+
+LEVELS = [level[0] for level in LEVEL_MAP]
+"""The list of level names."""
 
 DEFAULT_LOGGING_LEVEL = logging.DEBUG
+
+logging_level = getattr
 
 from inspy_logger.helpers import (
     translate_to_logging_level,
@@ -47,7 +69,12 @@ class Logger:
             self.__name = name
             self.logger = logging.getLogger(name)
             self.logger.setLevel(logging.DEBUG)
-            self.__console_level = console_level
+            determined_level = console_level
+
+            if isinstance(console_level, str):
+                determined_level = translate_to_logging_level(console_level)
+
+            self.__console_level = determined_level
             self.filename = filename
             self.__file_level = file_level or DEFAULT_LOGGING_LEVEL
             self.parent = parent
@@ -63,6 +90,10 @@ class Logger:
             self.set_up_console()
             self.set_up_file()
             self.children = []
+
+    @property
+    def device(self):
+        return self.logger
 
     @property
     def name(self):
@@ -115,8 +146,7 @@ class Logger:
                 child.set_level(file_level=file_level)
 
     def get_child(self, name=None, console_level=None, file_level=None):
-        self.logger.debug("Getting child logger")
-        console_level = console_level or DEFAULT_LOGGING_LEVEL
+        console_level = console_level or self.__console_level
         caller_frame = inspect.stack()[1]
 
         if name is None:
@@ -130,7 +160,7 @@ class Logger:
             if child.logger.name == child_logger_name:
                 return child
 
-        child_logger = Logger(child_logger_name, console_level, file_level, parent=self)
+        child_logger = Logger(name=child_logger_name, console_level=console_level, file_level=file_level, parent=self)
         self.children.append(child_logger)
         return child_logger
 
@@ -166,6 +196,19 @@ class Logger:
 
         return results
 
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+
     def __repr__(self):
         name = self.name
         hex_id = hex(id(self))
@@ -179,116 +222,52 @@ class Logger:
         if parent_part.endswith('|'):
             parent_part = str(parent_part[:-2])
 
-        return f'<Logger: {name} at {hex_id}{parent_part}>'
+        return f'<Logger: {name} w/ level {self.logger.level} at {hex_id}{parent_part}>'
 
 
-LOG_DEVICE = Logger(PROG_NAME, DEFAULT_LOGGING_LEVEL)
-MOD_LOG_DEVICE = LOG_DEVICE.get_child("log_engine")
+    @classmethod
+    def create_logger_for_caller(cls):
+        """
+        Creates a logger for the module that calls this method.
+
+        Returns:
+            Logger: An instance of the Logger class for the calling module.
+        """
+        frame = inspect.currentframe().f_back
+        if module_path := cls._determine_module_path(frame):
+            return cls(module_path)
+        else:
+            raise ValueError("Unable to determine module path for logger creation.")
+
+    @staticmethod
+    def _determine_module_path(frame):
+        """
+        Determines the in-project path of the module from the call frame.
+
+        Args:
+            frame: The frame from which to determine the module path.
+
+        Returns:
+            str: The in-project path of the module.
+        """
+        if module := inspect.getmodule(frame):
+            base_path = os.path.dirname(os.path.abspath(module.__file__))
+            relative_path = os.path.relpath(frame.f_code.co_filename, base_path)
+            return relative_path.replace(os.path.sep, '.').rstrip('.py')
+        return None
+
+
+
+found_level = find_variable_in_call_stack('INSPY_LOG_LEVEL', DEFAULT_LOGGING_LEVEL)
+
+
+LOG_DEVICE = Logger(PROG_NAME, found_level)
+MOD_LOG_DEVICE = LOG_DEVICE.get_child("log_engine", found_level)
 MOD_LOGGER = MOD_LOG_DEVICE.logger
 MOD_LOGGER.debug(f"Started logger for {__name__}.")
 
 add_child = LOG_DEVICE.get_child
 
-InspyLogger = LOG_DEVICE
+InspyLogger = Logger
 
-def _get_parent_logging_device():
-    """
-    Determines the parent logging device by inspecting the caller's log_device or parent_log_device attribute.
-
-    Returns:
-        Logger: The parent logging device.
-    """
-    MOD_LOGGER.debug("Determining parent logging device")
-    caller_frame = inspect.currentframe().f_back
-    caller_locals = caller_frame.f_locals
-
-    if "logger" in caller_locals:
-        return caller_locals["logger"]
-    elif "parent_log_device" in caller_locals:
-        return caller_locals["parent_log_device"]
-    else:
-        raise ValueError("Unable to determine the parent logging device.")
-
-
-class Loggable:
-    """
-    A metaclass to enhance classes with logging capabilities. Classes that inherit from
-    'Loggable' can instantly access a logger without manually setting it up. This logger
-    is derived from a parent logger, ensuring consistent logging behavior and hierarchy.
-
-    Attributes:
-        - log_device: The logger device associated with the instance of the class.
-    """
-
-    def __init__(self, parent_log_device=None, **kwargs):
-        self.parent_log_device = parent_log_device
-        self.__log_name = self.__class__.__name__
-        if self.parent_log_device is not None:
-            self.__log_device = self.parent_log_device.get_child(
-                self.__class__.__name__
-            )
-        else:
-            self.__log_device = _get_parent_logging_device().get_child(
-                self.__class__.__name__
-            )
-
-    @property
-    def log_device(self):
-        return self.__log_device
-
-    @log_device.setter
-    def log_device(self, new):
-        if not isinstance(new, Logger):
-            raise TypeError('log_device must be of type "Logger"')
-
-        self.__log_device = new
-
-    def create_child_logger(self, name=None, override=False):
-        """
-        Creates and returns a child logger of this object's logger.
-
-        Parameters:
-            name (str, optional): The name of the child logger.
-                If not provided, the name of the calling function is used.
-            override (bool, optional): A flag to override the membership check. Defaults to False.
-
-        Returns:
-            Logger: An instance of the Logger class that represents the child logger.
-        """
-        if not override:
-            self.__is_member__()
-
-        if name is None:
-            name = inspect.stack()[1][
-                3
-            ]  # Get the name of the calling function if no name is provided
-
-        return self.log_device.get_child(name)
-
-    def __is_member__(self):
-        """
-        Checks whether the caller of this method is a member of the same class.
-
-        Raises:
-            PermissionError: If the caller of this method is not a member of the same class.
-        """
-        log_device = self.log_device.get_child("__is_member__")
-        log = log_device.logger
-
-        current_frame = inspect.currentframe()
-        log.debug(f"Current frame: {current_frame}")
-
-        caller_frame = current_frame.f_back
-        log.debug(f"Caller frame: {caller_frame}")
-
-        caller_self = caller_frame.f_locals.get("self", None)
-        log.debug(f"Caller self: {caller_self}")
-
-        log.debug("Checking if caller is a member of this class...")
-        if not isinstance(caller_self, self.__class__):
-            raise PermissionError(
-                "Access denied.\n"
-                f"Method can only be accessed by members of the same class. {caller_self.__class__.__name__} is not such a member"
-            )
-
-        log.debug(f"Access granted to {caller_self.__class__.__name__}")
+from inspy_logger.helpers.base_classes import Loggable
